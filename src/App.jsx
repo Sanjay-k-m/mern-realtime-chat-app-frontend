fb9d0d5004f23e11f6bc8c22d8d1b379480b5f2b
Sanjay-k-m <sanjay_k.m@outlook.com>
2024-10-04 08:04:37 +0530

    signup , login page ui design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <button className="btn">Button</button>
-<button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
-    </>
-  )
+    <div className="p-4 h-screen flex item-center justify-center">
+      {/* <Login /> */}
+      <SignUp />
+    </div>
+  );
 }
 
-export default App
+export default App;
